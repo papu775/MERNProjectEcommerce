c5372c7075f501c44df4a09187aa8b6223aa4e99
Soubhik Laha <soubhiklaha848551@gmail.com>
2022-04-18 11:08:11 +0530

    Setup Database

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ app.use(express.json());
 //Route Imports
 const product = require('./router/productRoute');
 
+//Route
 app.use('/api/v1/',product);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const dotenv = require('dotenv');
 const app = require('./app');
-
+const connectDatabase = require('./database');
 //config
 dotenv.config({path:'./backend/config/config.env'});
+
+connectDatabase();
 
 app.listen(process.env.PORT,()=>{
     console.log(`server is working on http://localhost:${process.env.PORT}`);
